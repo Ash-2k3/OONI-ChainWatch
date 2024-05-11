049801b1419fba755a08cc0d4421fceb2b73d70e
Ash-2k3 <ash.me.cse@gmail.com>
2024-05-11 23:42:04 +0530

    Transform json ooni data to python DS using json.load() method

---
+++ main.py
@@ -0,0 +1,13 @@
+import json
+
+def load_json(fileName):
+           data = {}
+           with open(fileName, 'r') as f:
+                     data = json.load(f)
+           return data
+
+ooni_data = load_json("datasets/1.json")
+
+print('Here is the ooni_data *****************')
+
+print(ooni_data)
