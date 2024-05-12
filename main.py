8240eba68db82dbd404bba0e83e0667cd901ff69
Ash-2k3 <ash.me.cse@gmail.com>
2024-05-12 09:48:35 +0530

    Fetch OONI data using AWS S3 bucket

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
+import boto3
 import json
+import gzip
+from dotenv import load_dotenv
+import os
 
-def load_json(fileName):
-           data = {}
-           with open(fileName, 'r') as f:
-                     data = json.load(f)
-           return data
+# python-dotenv, bot3
 
-ooni_data = load_json("datasets/1.json")
+load_dotenv()
+
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY_PASSWORD")
+
+s3 = boto3.client('s3',
+                  aws_access_key_id=AWS_ACCESS_KEY_ID,
+                  aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+
+
+bucket_name = 'ooni-data-eu-fra'
+
+response = s3.list_objects_v2(Bucket=bucket_name)
+
+file_count = 0
+for obj in response.get('Contents', []):
+    print(obj['Key'])
+    file_count += 1
+    if file_count >= 10:
+        break
