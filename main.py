46d66b90f430d5587fab6745365cf2778a3881a1
Ash-2k3 <ash.me.cse@gmail.com>
2024-05-12 11:20:37 +0530

    Download and Parse measurements from OONI S3 data keys

--- main.py
+++ main.py
@@ -20,9 +20,31 @@ bucket_name = 'ooni-data-eu-fra'
 
 response = s3.list_objects_v2(Bucket=bucket_name)
 
+def download_and_parse_measurement(bucket_name, object_key):
+           """Downloads and parses a single measurement from S3."""
+           try: 
+                      obj = s3.get_object(Bucket=bucket_name, Key=object_key)
+                      with gzip.GzipFile(fileobj=obj['Body']) as gzip_file:
+                                 measurement_data = json.load(gzip_file)
+                      return measurement_data
+           except Exception as e:
+                      print(f"Error downloading or parsing file {object_key}: {e}")
+
 file_count = 0
 for obj in response.get('Contents', []):
-    print(obj['Key'])
-    file_count += 1
-    if file_count >= 10:
-        break
+    file_key = obj['Key']
+    # Check if the file is gzipped and in json convertible format.
+    if file_key.endswith('.json.gz'):
+        print('*************************************')
+        print(obj['Key'])
+        try:
+            # Download and parse the measurement data
+            measurement_data = download_and_parse_measurement(bucket_name, obj['Key'])
+            print(measurement_data)
+        except Exception as e:
+            print(f"Error processing file {file_key}: {e}")
+        file_count += 1
+        if file_count >= 10:
+            break
+    else:
+        print(f"Skipping non-gzipped file: {file_key}")
